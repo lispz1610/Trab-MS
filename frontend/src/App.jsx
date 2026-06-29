1fc220e1ca6504d66535d400de4effb7939f5ecc
thamirisbalbi <38821416+thamirisbalbi@users.noreply.github.com>
2026-06-29 16:49:27 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
-import Login from './pages/Login';
+import React, { useState } from 'react';
+import Login from "./pages/Login";
+import Home from "./pages/Home"; 
 
-function App() {
-  return <Login />;
+export default function App() {
+ //
+ // comenta para teste:
+    const [usuario, setUsuario] = useState(() => {
+    //comenta para teste:
+    const usuarioSalvo = localStorage.getItem("usuario");
+    //descomenta para teste:
+   //teste const [usuario, setUsuario] = useState({ nome: "User", email: "teste@rabisco.com" });
+    //comenta para teste://
+    return usuarioSalvo ? JSON.parse(usuarioSalvo) : null;
+});
+
+  const fazerLogout = () => {
+    localStorage.removeItem("usuario");
+    setUsuario(null);
+  };
+
+  if (!usuario) {
+    return <Login onLoginSucesso={setUsuario} />;
+  }
+
+  return <Home usuario={usuario} onLogout={fazerLogout} />;
 }
-
-export default App;
